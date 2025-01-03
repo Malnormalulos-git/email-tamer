a8750454e8cd85a70711b564fbce8b4bfa88d452
Malnormalulos-git <malnormalulo1@gmail.com>
2025-01-03 23:43:44 +0200

    backend: logging (Serilog)

--- backend/EmailTamer.Application/Program.cs
+++ backend/EmailTamer.Application/Program.cs
@@ -1,6 +1,35 @@
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Host.UseSerilog((context, loggerConfiguration) =>
+{
+    const string logOutputTemplate = "[{Timestamp:HH:mm:ss.fff}] "
+                                     + "[{RequestId}] "
+                                     + "[{SourceContext:l}] "
+                                     + "[{Level:u3}] "
+                                     + "{Message:lj}{NewLine}{Exception}";
+
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .Enrich.FromLogContext()
+        .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+        .Enrich.WithThreadId();
+
+    loggerConfiguration.WriteTo.Console(
+        outputTemplate: logOutputTemplate,
+        theme: AnsiConsoleTheme.Literate,
+        restrictedToMinimumLevel: LogEventLevel.Debug);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSerilogRequestLogging();
+
+
 
 app.Run();
