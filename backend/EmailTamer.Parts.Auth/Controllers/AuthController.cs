ace923b33de0a1762b922440ad360fe69b179cb5
Malnormalulos-git <malnormalulo1@gmail.com>
2025-03-14 14:32:13 +0200

    backend: formalized auth controller

--- backend/EmailTamer.Parts.Auth/Controllers/AuthController.cs
+++ backend/EmailTamer.Parts.Auth/Controllers/AuthController.cs
@@ -3,39 +3,31 @@ using EmailTamer.Auth.Operations.Commands;
 using EmailTamer.Auth.Operations.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmailTamer.Auth.Controllers;
 
 [Route("api/auth")]
-public class AuthController : Controller
+public class AuthController(IMediator mediator) : Controller
 {
-    private readonly IMediator _mediator;
-
-    public AuthController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
-
-    [HttpPost("register", Name = "Register")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status409Conflict)]
-    public Task<IActionResult> СreateUser([FromBody] CreateUserDto dto, CancellationToken ct = default) =>
-        _mediator.Send(new CreateUser(dto), ct);
+    [HttpPost("register", Name = nameof(Register))]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(409)]
+    public Task<IActionResult> Register([FromBody] CreateUserDto dto, CancellationToken ct = default) =>
+        mediator.Send(new CreateUser(dto), ct);
     
-    [HttpPost("login", Name = "LogIn")]
-    [ProducesResponseType(typeof(AuthResponseDto), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [HttpPost("login", Name = nameof(Login))]
+    [ProducesResponseType(typeof(AuthResponseDto), 200)]
+    [ProducesResponseType(401)]
     public Task<IActionResult> Login([FromBody] LogInDto dto, CancellationToken ct = default) =>
-        _mediator.Send(new LogInCommand(dto), ct);
+        mediator.Send(new LogInCommand(dto), ct);
     
     
     [Authorize]
-    [HttpGet("user", Name = "GetCurrentUser")]
-    [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
-    public Task<IActionResult> CurrentUser(CancellationToken ct = default) =>
-        _mediator.Send(new GetCurrentUser(), ct);
+    [HttpGet("user", Name = nameof(GetCurrentUser))]
+    [ProducesResponseType(typeof(UserDto), 200)]
+    public Task<IActionResult> GetCurrentUser(CancellationToken ct = default) =>
+        mediator.Send(new GetCurrentUser(), ct);
 
 }
