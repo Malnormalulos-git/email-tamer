ff76bf8844cf769f0d4d1feda0b5722e6a29568f
Malnormalulos-git <malnormalulo1@gmail.com>
2025-04-08 17:55:59 +0300

    backend: AddThreadIdToMessages tenant db migration

---
+++ backend/EmailTamer.Database.Tenant/Migrations/20250408144644_AddThreadIdToMessages.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmailTamer.Database.Tenant.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddThreadIdToMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ThreadId",
+                table: "Messages",
+                type: "varchar(255)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ThreadId",
+                table: "Messages",
+                column: "ThreadId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_ThreadId",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "ThreadId",
+                table: "Messages");
+        }
+    }
+}
