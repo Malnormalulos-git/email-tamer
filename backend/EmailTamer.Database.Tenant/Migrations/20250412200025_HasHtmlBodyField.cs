5a3c444a83f5dabc59e1759e8b6ed849d532915d
Malnormalulos-git <malnormalulo1@gmail.com>
2025-04-12 23:09:02 +0300

    backend: HasHtmlBodyField migration for tenant db

---
+++ backend/EmailTamer.Database.Tenant/Migrations/20250412200025_HasHtmlBodyField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmailTamer.Database.Tenant.Migrations
+{
+    /// <inheritdoc />
+    public partial class HasHtmlBodyField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasHtmlBody",
+                table: "Messages",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasHtmlBody",
+                table: "Messages");
+        }
+    }
+}
