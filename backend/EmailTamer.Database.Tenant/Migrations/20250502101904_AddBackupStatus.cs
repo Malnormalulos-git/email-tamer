a41f33b189285af9bb1d643d2449bc8d7536310a
Malnormalulos-git <malnormalulo1@gmail.com>
2025-05-02 13:56:42 +0300

    backend: AddBackupStatus migration for tenant db

---
+++ backend/EmailTamer.Database.Tenant/Migrations/20250502101904_AddBackupStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmailTamer.Database.Tenant.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBackupStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BackupStatus",
+                table: "EmailBoxes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BackupStatus",
+                table: "EmailBoxes");
+        }
+    }
+}
