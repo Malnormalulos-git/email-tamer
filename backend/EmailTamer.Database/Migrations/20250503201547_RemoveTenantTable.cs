6a512412eeb7d00c7ecb2c6826fcbdbf52aea0c4
Malnormalulos-git <malnormalulo1@gmail.com>
2025-05-03 23:37:13 +0300

    backend: RemoveTenantTable migration for main db

---
+++ backend/EmailTamer.Database/Migrations/20250503201547_RemoveTenantTable.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmailTamer.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveTenantTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Tenants_TenantId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "Tenants");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_TenantId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "TenantId",
+                table: "AspNetUsers",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "char(36)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "TenantId",
+                table: "AspNetUsers",
+                type: "char(36)",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "char(36)");
+
+            migrationBuilder.CreateTable(
+                name: "Tenants",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "char(36)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tenants", x => x.Id);
+                    table.CheckConstraint("CHK_Tenants_IdNotDefault", "\"Id\" <> '00000000-0000-0000-0000-000000000000'");
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_TenantId",
+                table: "AspNetUsers",
+                column: "TenantId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Tenants_TenantId",
+                table: "AspNetUsers",
+                column: "TenantId",
+                principalTable: "Tenants",
+                principalColumn: "Id");
+        }
+    }
+}
