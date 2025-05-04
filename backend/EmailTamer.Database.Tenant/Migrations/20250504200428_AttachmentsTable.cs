ef6f61c6bb03effb674365b28dba9d32b955264e
Malnormalulos-git <malnormalulo1@gmail.com>
2025-05-04 23:29:16 +0300

    backend: AttachmentsTable migration for tenant db

---
+++ backend/EmailTamer.Database.Tenant/Migrations/20250504200428_AttachmentsTable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmailTamer.Database.Tenant.Migrations
+{
+    /// <inheritdoc />
+    public partial class AttachmentsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AttachmentFilesNames",
+                table: "Messages");
+
+            migrationBuilder.CreateTable(
+                name: "Attachments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false),
+                    FileName = table.Column<string>(type: "longtext", nullable: false),
+                    MessageId = table.Column<string>(type: "varchar(255)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attachments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Attachments_Messages_MessageId",
+                        column: x => x.MessageId,
+                        principalTable: "Messages",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attachments_MessageId",
+                table: "Attachments",
+                column: "MessageId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attachments");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AttachmentFilesNames",
+                table: "Messages",
+                type: "longtext",
+                nullable: false);
+        }
+    }
+}
